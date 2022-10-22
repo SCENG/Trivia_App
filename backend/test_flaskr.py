77a2d81eb0ee07a793dffe76e5d21b1397c830dd
Elijah Sinkamba <chisanga110@live.com>
2022-10-22 08:08:18 +0200

    test bad request

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -56,12 +56,46 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertEqual(data['deleted'], 1)
 
+    def test_create_question(self):
+        response = self.client().post('/questions', json={'question': 'test', 'answer': 'test', 'difficulty': 1, 'category': 1})
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+
+    def test_search_questions(self):
+        response = self.client().post('/questions/search', json={'searchTerm': 'test'})
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+    
+    def test_get_questions_by_category(self):
+        response = self.client().get('/categories/1/questions')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+        self.assertEqual(data['current_category'], 1)
+
+
     def test_trivia(self):
         response = self.client().post('/quizzes', json={})
         data = json.loads(response.data)
         self.assert_(response.status_code, 422)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'unprocessable')
+
+    def test_bad_request(self):
+        response = self.client().get('/categories/100/questions')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+    
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
